6c75bfd498926d05770b45f98a2b1ea40fea9cc5
David Zeng <zeng_d@epitech.eu>
2016-04-06 00:56:57 +0200

    [Added]Pipe

--- src/exec_process.c
+++ src/exec_process.c
@@ -5,35 +5,47 @@
 ** Login   <zeng_d@epitech.net>
 **
 ** Started on  Tue Apr  5 21:54:14 2016 David Zeng
-** Last update Tue Apr  5 23:38:20 2016 David Zeng
+** Last update Wed Apr  6 00:56:17 2016 David Zeng
 */
 
 #include "my_fonction.h"
 
-static void	fork_pipe(t_proc *proc, char ***env)
+static void	fork_status(void)
 {
-  int		pid;
   int		status;
 
-  pid = fork();
-  if (pid == 0)
+  wait(&status);
+  if (WIFSIGNALED(status))
     {
+      if (WTERMSIG(status) == SIGSEGV)
+	my_put_err("Segmentation fault\n");
+      else if (WTERMSIG(status) == SIGFPE)
+	my_put_err("Floating point exception\n");
+      else if (WTERMSIG(status) == SIGABRT)
+	my_put_err("Aborted\n");
+    }
+}
+
+static void	fork_pipe(t_proc *proc, char ***env, int *fd_in, int *fd)
+{
+  int		pid;
+
+  if ((pid = fork()) == 0)
+    {
+      dup2(*fd_in, 0);
+      if (proc->next != NULL)
+	dup2(fd[1], 1);
+      //redir_proc(proc, )
+      close(fd[0]);
       if (my_builtins(proc->argv, env, true) != 1)
 	my_get_exec(*env, proc->argv);
       exit(1);
     }
   else if (pid > 0)
     {
-      wait(&status);
-      if (WIFSIGNALED(status))
-	{
-	  if (WTERMSIG(status) == SIGSEGV)
-	    my_put_err("Segmentation fault\n");
-	  else if (WTERMSIG(status) == SIGFPE)
-	    my_put_err("Floating point exception\n");
-	  else if (WTERMSIG(status) == SIGABRT)
-	    my_put_err("Aborted\n");
-	}
+      fork_status();
+      close(fd[1]);
+      *fd_in = fd[0];
     }
   else
     my_put_err("Fork failed.\n");
@@ -42,14 +54,18 @@ static void	fork_pipe(t_proc *proc, char ***env)
 static void	exec_pipe(t_proc *proc, char ***env)
 {
   t_proc	*tmp;
+  int		fd_in;
+  int		fd[2];
 
   tmp = proc;
+  fd_in = 0;
   while (tmp != NULL)
     {
-      if (my_builtins(tmp->argv, env, false) == 1 && tmp->next == NULL)
-	my_builtins(proc->argv, env, true);
+      pipe(fd);
+      if (tmp->next == NULL && my_builtins(tmp->argv, env, false) == 1)
+	my_builtins(tmp->argv, env, true);
       else
-	fork_pipe(tmp, env);
+	fork_pipe(tmp, env, &fd_in, fd);
       tmp = tmp->next;
     }
 }
